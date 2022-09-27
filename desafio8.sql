2e0c6c75c4e726dee41bc89186d5219f0970a03f
João Victor - Turma 22 - Tribo A <jvvr2013@gmail.com>
2022-09-26 21:05:32 -0300

    Delete desafio8.sql

--- desafio8.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	a.artist AS artista,
-    ab.album AS album
-FROM SpotifyClone.artist AS a
-	INNER JOIN SpotifyClone.albums AS ab ON ab.artist_id = a.artist_id
-WHERE a.artist LIKE 'Elis Regina';
